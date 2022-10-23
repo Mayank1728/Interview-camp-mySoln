fcaba0fdf1e4c7024fab1ab2d2347d62fbeb93e4
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-10-23 23:31:13 +0530

    updates

--- 10-slidingWindow.js
+++ 10-slidingWindow.js
@@ -1,22 +1,42 @@
 // Subarray sliding window
+//index :  0  1  2  3  4  5  6  7
+//arr     [5, 3, 1, 7, 6, 4, 2, 3], 14 => [2, 4]
 
 function slidingWindow(arr, target = 0) {
   let start = 0;
-  let end = start + 1;
-  let sum = arr[start] + arr[end];
+  let end = 0;
+  let sum = arr[start];
   const len = arr.length;
-  while (end < len) {
+  while (start < len) {
     if (sum === target) {
       console.log('true');
+      console.log(start, end);
       return true;
     } else if (sum < target) {
-      end++;
-      sum += arr[end];
+      // Edge case 1: when sum is too low but your end pointer
+      //              is on last index incrementing it now will
+      //              make it out of bounds
+      if (end + 1 > len) {
+        console.log('End pointer went out of bounds!!');
+        return false;
+      }
+      // first increment end pointer
+      // then add the vale present at end pointer
+      sum = sum + arr[++end];
     } else {
-      sum -= arr[start];
-      start++;
+      // first remove the arr[start] value from the sum
+      // then increment the start pointer.
+      sum = sum - arr[start++];
+    }
+    // Edge case 2 : when start becomes greater than end
+    //               end = start and sum is reinitialized to arr[sum]
+    if (start > end) {
+      end = start;
+      sum = arr[start];
     }
   }
+  // Target cannot be created so return false.
+  console.log('Value cannot be created!!');
   return false;
 }
 slidingWindow([5, 3, 1, 7, 6, 4, 2, 3]);
