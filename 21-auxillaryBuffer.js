df9e1dfa02439a61a4ea96df0a7973df4fa59cad
Mayank Mudgal <71082230+Mayank1728@users.noreply.github.com>
2022-11-01 01:30:04 -0700

    Update 21-auxillaryBuffer.js

---
+++ 21-auxillaryBuffer.js
@@ -0,0 +1,19 @@
+// Function combination to print all the combination of array with a particular size
+// ['a', 'b', 'c'] , 2 (should print)=> [[a, b], [a, c], [b, c]]
+
+function combinations(arr, size, buffer = new Array(size), startIndex = 0, bufferIndex = 0){
+  if(bufferIndex === size){
+    console.log(buffer);
+    return;
+  }
+  if(startIndex === arr.length){
+    return;
+  }
+  for(let i = startIndex; i < arr.length; i++){
+    buffer[bufferIndex] = arr[i];
+    combinations(arr, size, buffer, i + 1, bufferIndex + 1);
+  }
+  // Time : O()
+  // Space : O()
+}
+combinations([1, 2, 3, 4, 5], 2)
